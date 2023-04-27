72a9998787c8a666d4b1edb71e7c9c2be2d943ed
NikolasMenezes <111708104+NikolasMenezes@users.noreply.github.com>
2023-04-27 10:40:30 -0300

    game working

--- js/game.js
+++ js/game.js
@@ -1,11 +1,34 @@
-const mario = document.querySelector("#mario")
+const mario = document.querySelector(".mario")
+const pipe = document.querySelector(".pipe")
+const clouds = document.querySelector(".clouds")
 
-const jump = () =>{
+const marioJump = () =>{
   mario.classList.add("jump")
 
-  setInterval(() => {
-    mario.classList.remove("jump")
-  }, 700)
+  setTimeout(() => mario.classList.remove("jump"), 500)
 }
 
-window.addEventListener("keydown", jump)
+const gameLoop = setInterval(() => {
+
+  const pipePosition = pipe.offsetLeft;
+  const cloudsPosition = clouds.offsetLeft;
+  const marioPosition = Number(window.getComputedStyle(mario).bottom.replace('px', ''))
+  
+  if(pipePosition <= 100 && pipePosition > 0 && marioPosition < 78){
+
+    pipe.style.animation = 'none'
+    clouds.style.animation = 'none'
+    pipe.style.left = `${pipePosition}px`
+    clouds.style.left = `${cloudsPosition}px`
+
+    mario.setAttribute("src", "./img/game-over.webp")
+    mario.style.height= '90px'
+    mario.style.left= `${pipePosition - 20}px`
+    mario.style.bottom= `${marioPosition}px`
+    
+  }
+
+
+}, 10)
+
+document.addEventListener("keydown", marioJump)
